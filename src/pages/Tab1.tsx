fb7eb78771a454c836088446252a19b41ad0f575
João Vitor <joaovcostaandrade@gmail.com>
2022-08-17 20:16:18 -0300

    Correção

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -10,13 +10,73 @@ const Tab1: React.FC = () => {
           <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent>
+      
+      <IonGrid>
+    
+        <IonRow>
+          <IonCol >
+            <IonButton  color="danger">1</IonButton>
+          </IonCol>
+          <IonCol>
+          <IonButton color="danger">2</IonButton>
+          </IonCol>
+          <IonCol>
+            <IonButton color="danger">3</IonButton>
+          </IonCol>
+          <IonCol>
+            <IonButton color="warning">/</IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+            <IonButton color="danger">4</IonButton>
+          </IonCol>
+
+          <IonCol>
+            <IonButton color="danger">5</IonButton>
+          </IonCol>
+
+          <IonCol>
+            <IonButton color="danger">6</IonButton>
+          </IonCol>
+
+          <IonCol>
+            <IonButton color="warning">x</IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+            <IonButton color="danger">7</IonButton>
+          </IonCol>
+
+          <IonCol>
+            <IonButton color="danger">8</IonButton>
+          </IonCol>
+
+          <IonCol>
+            <IonButton color="danger">9</IonButton>
+          </IonCol>
+
+          <IonCol>
+            <IonButton color="warning">-</IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+        <IonCol>
+            <IonButton color="warning">C</IonButton>
+          </IonCol>
+          <IonCol>
+            <IonButton color="danger">0</IonButton>
+          </IonCol>
+          <IonCol>
+            <IonButton color="warning">+</IonButton>
+          </IonCol>
+          <IonCol>
+            <IonButton color="warning">=</IonButton>
+          </IonCol> 
+        </IonRow>
+      </IonGrid>        
       </IonContent>
     </IonPage>
   );
